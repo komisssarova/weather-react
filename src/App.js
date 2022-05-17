14a2c2c18ddf1dcbbd5b98f5ac409ad1e73f8bd5
komisssarova <komisssarova.uk@gmail.com>
2022-05-17 17:48:10 +0100

    adding all packages and js

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Weather from "./Weather";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello world!!!</h1>
+        <Weather city="London" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
