6825f238a6acb3d46711e449a0c51c6481b3c1fe
komisssarova <komisssarova.uk@gmail.com>
2022-05-24 11:44:08 +0100

    committed changes in html and js

--- src/index.js
+++ src/index.js
@@ -10,6 +10,34 @@ function App() {
     <div className="App">
       <h1>Weather App</h1>
       <WeatherSearch />
+      <footer>
+        <small>
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/kateryna-komisarova/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Kateryna Komisarova{" "}
+          </a>
+          and is{" "}
+          <a
+            href="https://github.com/komisssarova/weather-react"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub{" "}
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://vermillion-tartufo-fbb7c7.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </small>
+      </footer>
     </div>
   );
 }
